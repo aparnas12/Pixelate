d5570b1ab64344fffb38d224b915bb3b0d0ce186
Evan Walker <ewalker3764@gmail.com>
2023-03-27 12:00:54 -0400

    more changes pt1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Your code here
+const pixelTable = document.getElementById('pixel-table');
+function makeRow () {
+    const tr = document.createElement('tr');
+    for (let i = 0; i < 20; i++) {
+    const td = document.createElement('td');
+    tr.appendChild(td);
+    return tr;
+}
+let newRow = makeRow();
+    pixelTable.appendChild(newRow);
+}
