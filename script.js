27164c1ae3496a2b87b9fbd2c478f9dedddfaacd
Aparna Priyavadan <aparnapri@Aparnas-MacBook-Pro.local>
2023-03-27 13:19:07 -0400

    button works

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Your code here
 const pixelTable = document.getElementById('pixel-table');
-function makeRow () {
+function makeRow() {
     const tr = document.createElement('tr');
     for (let i = 0; i < 20; i++) {
-    const td = document.createElement('td');
-    tr.appendChild(td);
-    pixelTable.appendChild(tr);
-}
+        const td = document.createElement('td');
+        tr.appendChild(td);
+        pixelTable.appendChild(tr);
+    }
 
-} 
-makeRow ();
-makeRow ();
-makeRow ();
+}
+makeRow();
+makeRow();
+makeRow();
+
+const rowButton = document.getElementById("add-row");
+
+rowButton.addEventListener('click',() => {makeRow()});
